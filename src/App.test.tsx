40a9af23cfddf45d5c2f300103c9b7c4ebf5e3ed
Richard <richard.m.hpa@gmail.com>
2022-09-06 22:15:10 +1200

    updated failing tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Hello world!', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/Hello world!/i)).toBeInTheDocument();
 });
