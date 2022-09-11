5fc29711375b7721a21d9f17bb207af2ce2647f8
Richard <richard.m.hpa@gmail.com>
2022-09-11 12:11:48 +1200

    test octokit api

--- .github/actions/compare-versions/index.js
+++ .github/actions/compare-versions/index.js
@@ -2,14 +2,24 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet')
-  console.log(`Hello ${nameToGreet}!`)
-  const time = new Date().toTimeString()
-  core.setOutput('time', time)
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`)
+  // // `who-to-greet` input defined in action metadata file
+  // const nameToGreet = core.getInput('who-to-greet')
+  // console.log(`Hello ${nameToGreet}!`)
+  // const time = new Date().toTimeString()
+  // core.setOutput('time', time)
+  // // Get the JSON webhook payload for the event that triggered the workflow
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // console.log(`The event payload: ${payload}`)
+  const myToken = process.env.GITHUB_TOKEN
+  const octokit = github.getOctokit(myToken)
+
+  const { data: releases } = await octokit.rest.repos.listReleases({
+    owner: 'RichardHpa',
+    repo: 'Demo-App',
+    per_page: 2,
+  })
+
+  console.log(releases)
 } catch (error) {
   core.setFailed(error.message)
 }
