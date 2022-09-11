0d5a64780650ce0bb078568a790311fbdb436bef
Richard <richard.m.hpa@gmail.com>
2022-09-11 15:06:12 +1200

    removed uses

--- .github/actions/compare-versions/index.js
+++ .github/actions/compare-versions/index.js
@@ -4,14 +4,6 @@ const semver = require('semver')
 
 async function run() {
   try {
-    // // `who-to-greet` input defined in action metadata file
-    // const nameToGreet = core.getInput('who-to-greet')
-    // console.log(`Hello ${nameToGreet}!`)
-    // const time = new Date().toTimeString()
-    // core.setOutput('time', time)
-    // // Get the JSON webhook payload for the event that triggered the workflow
-    // const payload = JSON.stringify(github.context.payload, undefined, 2)
-    // console.log(`The event payload: ${payload}`)
     const myToken = process.env.GITHUB_TOKEN
     const octokit = github.getOctokit(myToken)
 
