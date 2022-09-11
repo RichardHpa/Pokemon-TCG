a44ac944c0e895d120d5b64f08d86469857c8a13
Richard <richard.m.hpa@gmail.com>
2022-09-11 12:30:55 +1200

    fixed console.log

--- .github/actions/compare-versions/index.js
+++ .github/actions/compare-versions/index.js
@@ -19,7 +19,7 @@ async function run() {
       repo: 'Demo-App',
       per_page: 1,
     })
-    console.log`The current package.json version is ${core.getInput('currentVersion')}`
+    console.log(`The current package.json version is ${core.getInput('currentVersion')}`)
     console.log(`The latest release: ${releases[0].tag_name}`)
   } catch (error) {
     core.setFailed(error.message)
