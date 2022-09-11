e1ae333246c3fb3b4aeb3f7f92cb8699ba8bd786
Richard Hpa <richard.m.hpa@gmail.com>
2022-09-12 07:51:10 +1200

    Formatted Build and Deploy Action (#9)
    Added checks to build and deploy actions. The Deploy steps will only run if the branch you have pushed to has a newer version of the package.json file

---
+++ .github/actions/compare-versions/index.js
@@ -0,0 +1,36 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const semver = require('semver')
+
+async function run() {
+  try {
+    const myToken = process.env.GITHUB_TOKEN
+    const octokit = github.getOctokit(myToken)
+
+    const currentVersion = semver.parse(core.getInput('currentVersion'))
+    if (!currentVersion?.version) {
+      throw new Error(`Could not parse a version out of the package.json.`)
+    }
+
+    const { data: releases } = await octokit.rest.repos.listReleases({
+      owner: 'RichardHpa',
+      repo: 'Demo-App',
+      per_page: 1,
+    })
+
+    const latestVersion = semver.parse(releases[0].tag_name)
+    if (!latestVersion?.version) {
+      throw new Error(`Could not parse a version from octokit api.`)
+    }
+
+    const isNewerVersion = semver.gt(currentVersion.version, latestVersion.version)
+    core.setOutput('isNewerVersion', isNewerVersion)
+
+    const versionType = semver.diff(currentVersion.version, latestVersion.version)
+    core.setOutput('versionType', versionType)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+run()
