18ef33399d8379a47b4894e68ea9211a272ec11b
Richard <richard.m.hpa@gmail.com>
2022-09-11 12:13:34 +1200

    wrapped in function

--- .github/actions/compare-versions/index.js
+++ .github/actions/compare-versions/index.js
@@ -1,25 +1,29 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
 
-try {
-  // // `who-to-greet` input defined in action metadata file
-  // const nameToGreet = core.getInput('who-to-greet')
-  // console.log(`Hello ${nameToGreet}!`)
-  // const time = new Date().toTimeString()
-  // core.setOutput('time', time)
-  // // Get the JSON webhook payload for the event that triggered the workflow
-  // const payload = JSON.stringify(github.context.payload, undefined, 2)
-  // console.log(`The event payload: ${payload}`)
-  const myToken = process.env.GITHUB_TOKEN
-  const octokit = github.getOctokit(myToken)
+async function run() {
+  try {
+    // // `who-to-greet` input defined in action metadata file
+    // const nameToGreet = core.getInput('who-to-greet')
+    // console.log(`Hello ${nameToGreet}!`)
+    // const time = new Date().toTimeString()
+    // core.setOutput('time', time)
+    // // Get the JSON webhook payload for the event that triggered the workflow
+    // const payload = JSON.stringify(github.context.payload, undefined, 2)
+    // console.log(`The event payload: ${payload}`)
+    const myToken = process.env.GITHUB_TOKEN
+    const octokit = github.getOctokit(myToken)
 
-  const { data: releases } = await octokit.rest.repos.listReleases({
-    owner: 'RichardHpa',
-    repo: 'Demo-App',
-    per_page: 2,
-  })
+    const { data: releases } = await octokit.rest.repos.listReleases({
+      owner: 'RichardHpa',
+      repo: 'Demo-App',
+      per_page: 2,
+    })
 
-  console.log(releases)
-} catch (error) {
-  core.setFailed(error.message)
+    console.log(releases)
+  } catch (error) {
+    core.setFailed(error.message)
+  }
 }
+
+run()
