dff8d40a9b5f1dd811cae24b2007f7dc570efa07
Richard <richard.m.hpa@gmail.com>
2022-09-12 19:08:27 +1200

    fixed

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,5 +4,5 @@ import App from './App'
 
 test('renders Hello world!', () => {
   renderWithRouter(<App />)
-  expect(screen.getByText('Hello world!!!')).toBeInTheDocument()
+  expect(screen.getByText('Hello world!!')).toBeInTheDocument()
 })
