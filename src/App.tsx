55cef0cf0339d21ac0714a7dfb95d5f72bb0c31d
Richard <richard.m.hpa@gmail.com>
2022-09-15 14:07:37 +1200

    create button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import { Routes, Route, Link } from 'react-router-dom'
+import { Button } from 'components/Button'
+import { useTheme, Theme } from 'providers/ThemeProvider'
 
 function App() {
+  const [theme, setTheme] = useTheme()
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT))
+  }
+
   return (
     <div className='bg-white dark:bg-gray-900 text-black dark:text-gray-200 min-h-screen'>
       <div className='container mx-auto py-6'>
         <h1 className='text-3xl font-bold underline'>Hello world!!</h1>
+        <Button onClick={toggleTheme}>{theme}</Button>
         <ul>
           <li>
             <Link to='/'>Home</Link>
@@ -17,7 +26,7 @@ function App() {
           </li>
         </ul>
         <hr />
-        {/* <div className='bg-white dark:bg-black'>Test</div> */}
+
         <Routes>
           <Route path='/' element={<>Home</>} />
           <Route path='about' element={<>About</>} />
