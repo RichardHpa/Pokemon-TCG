4189c677b94f65fe844afe3c9c51cb5deaf7f519
Richard <richard.m.hpa@gmail.com>
2022-09-26 18:55:56 +1300

    added footer into site

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from 'react-router-dom'
 
 import { Navbar } from 'components/Navbar'
+import { Footer } from 'components/Footer'
 
 function App() {
   return (
-    <div className='bg-white dark:bg-gray-900 text-black dark:text-gray-200 min-h-screen'>
+    <div className='bg-white dark:bg-gray-900 text-black dark:text-gray-200 min-h-screen flex flex-col'>
       <Navbar />
       <div className='container mx-auto py-6'>
         <Routes>
@@ -13,6 +14,7 @@ function App() {
           <Route path='*' element={<>404</>} />
         </Routes>
       </div>
+      <Footer />
     </div>
   )
 }
