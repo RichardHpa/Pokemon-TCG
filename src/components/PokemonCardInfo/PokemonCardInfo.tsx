26df7073cd40bbe8edfb89c6dbc6dc0b645449eb
Richard <richard.m.hpa@gmail.com>
2022-10-11 14:34:22 +1300

    added random card search

--- src/components/PokemonCardInfo/PokemonCardInfo.tsx
+++ src/components/PokemonCardInfo/PokemonCardInfo.tsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import type { PokemonCardInfoProps } from './types'
 
-export const PokemonCardInfo = ({ card }: PokemonCardInfoProps) => {
-  const { name, images, rarity, subtypes, flavorText } = card
+export const PokemonCardInfo = ({ card, loading }: PokemonCardInfoProps) => {
   const [imageLoaded, setImageLoaded] = useState(false)
+
+  useEffect(() => {
+    if (!card) {
+      setImageLoaded(false)
+    }
+  }, [card])
 
   const loadImage = () => {
     setTimeout(() => {
@@ -12,27 +17,28 @@ export const PokemonCardInfo = ({ card }: PokemonCardInfoProps) => {
     }, 500)
   }
 
+  const imageLoading = loading || !imageLoaded
   return (
     <div className='flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row  hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 '>
-      {imageLoaded ? null : (
-        <div role='status' className='animate-pulse w-[245px] h-[342px]'>
+      {imageLoading && (
+        <div role='status' className='animate-pulse min-w-[245px] h-[342px]'>
           <div className='flex justify-center items-center w-full h-full bg-gray-300 rounded dark:bg-gray-700' />
           <span className='sr-only'>Loading...</span>
         </div>
       )}
       <img
         style={imageLoaded ? {} : { display: 'none' }}
-        src={images.small}
-        alt={name}
+        src={card?.images.small}
+        alt={card?.name}
         onLoad={loadImage}
       />
 
       <div className='flex flex-col justify-between p-8 leading-normal'>
         <h5 className='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
-          {name && `${name} (${rarity})`}
+          {card?.name && `${card?.name} (${card?.rarity})`}
         </h5>
-        <p>{subtypes.join(', ')}</p>
-        <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>{flavorText}</p>
+        <p>{card?.subtypes.join(', ')}</p>
+        <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>{card?.flavorText}</p>
       </div>
     </div>
   )
