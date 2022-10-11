2e0d1624d7423ec04ad7701259d5c7f9674caa78
Richard <richard.m.hpa@gmail.com>
2022-10-11 14:10:49 +1300

    added pokemon Image card

---
+++ src/components/PokemonCardInfo/PokemonCardInfo.test.tsx
@@ -0,0 +1,31 @@
+import { screen, render } from '@testing-library/react'
+
+import { PokemonCardInfo } from './PokemonCardInfo'
+
+import { cards } from 'fixtures/cards'
+
+describe('PokemonCardInfo', () => {
+  test('should render the PokemonCardInfo component', () => {
+    render(<PokemonCardInfo card={cards[1]} />)
+    expect(screen.getByText('Aerodactyl (Rare Holo)')).toBeInTheDocument()
+  })
+
+  test('should render the PokemonCardInfo component with the correct image', () => {
+    render(<PokemonCardInfo card={cards[1]} />)
+    expect(screen.getByAltText('Aerodactyl')).toBeInTheDocument()
+  })
+
+  test('should render the PokemonCardInfo component with the correct subtypes', () => {
+    render(<PokemonCardInfo card={cards[1]} />)
+    expect(screen.getByText('Stage 1')).toBeInTheDocument()
+  })
+
+  test('should render the PokemonCardInfo component with the correct flavor text', () => {
+    render(<PokemonCardInfo card={cards[2]} />)
+    expect(
+      screen.getByText(
+        'Its poison stinger is very powerful. Its bright-colored body is intended to warn off its enemies.',
+      ),
+    ).toBeInTheDocument()
+  })
+})
