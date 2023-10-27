4dae401bd8779c518073157f6f21d9ea73b79df9
Richard <richard.m.hpa@gmail.com>
2023-10-27 20:44:36 +1300

    testing pr check

--- .github/actions/get-pr/index.js
+++ .github/actions/get-pr/index.js
@@ -5,30 +5,33 @@ async function run() {
   try {
     const myToken = process.env.GITHUB_TOKEN
     const octokit = github.getOctokit(myToken)
-    const currentSha = core.getInput('commitSha')
 
-    const { data } = await octokit.rest.repos.listPullRequestsAssociatedWithCommit({
-      owner: 'RichardHpa',
-      repo: 'Pokemon-TCG',
-      commit_sha: currentSha,
-    })
+    const number = github.context.payload.pull_request.number
+    console.log(number)
+    // const currentSha = core.getInput('commitSha')
 
-    let pullRequests = data
-    core.debug(`Found ${pullRequests.length} pull requests with the sha ${currentSha}.`)
+    // const { data } = await octokit.rest.repos.listPullRequestsAssociatedWithCommit({
+    //   owner: 'RichardHpa',
+    //   repo: 'Pokemon-TCG',
+    //   commit_sha: currentSha,
+    // })
 
-    if (!pullRequests.length) {
-      throw new Error(
-        `No pull requests found for ${github.context.repo.owner}/${github.context.repo.repo}@${currentSha}, Github Action failed.`,
-      )
-    }
+    // let pullRequests = data
+    // core.debug(`Found ${pullRequests.length} pull requests with the sha ${currentSha}.`)
 
-    let pullRequest = pullRequests[0]
-    setOutput('number', pullRequest.number)
+    // if (!pullRequests.length) {
+    //   throw new Error(
+    //     `No pull requests found for ${github.context.repo.owner}/${github.context.repo.repo}@${currentSha}, Github Action failed.`,
+    //   )
+    // }
+
+    // let pullRequest = pullRequests[0]
+    // setOutput('number', pullRequest.number)
     // setOutput('title', title || '')
     // setOutput('body', body || '')
-    setOutput('url', pullRequest.html_url || '')
-    setOutput('base-ref', pullRequest.base?.ref || '')
-    setOutput('base-sha', pullRequest.base?.sha || '')
+    // setOutput('url', pullRequest.html_url || '')
+    // setOutput('base-ref', pullRequest.base?.ref || '')
+    // setOutput('base-sha', pullRequest.base?.sha || '')
   } catch (error) {
     core.setFailed(error.message)
   }
