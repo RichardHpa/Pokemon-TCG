5ef9b68b1c76d9ce4c1e98a6c4bd2cf99b8a4037
Richard <richard.m.hpa@gmail.com>
2023-10-27 20:31:55 +1300

    getting branch name

--- .github/actions/get-pr/index.js
+++ .github/actions/get-pr/index.js
@@ -5,7 +5,7 @@ async function run() {
   try {
     const myToken = process.env.GITHUB_TOKEN
     const octokit = github.getOctokit(myToken)
-    const currentSha = core.getInput('commitSha', { required: false })
+    const currentSha = core.getInput('commitSha')
 
     const { data } = await octokit.rest.repos.listPullRequestsAssociatedWithCommit({
       owner: 'RichardHpa',
@@ -17,15 +17,15 @@ async function run() {
     core.debug(`Found ${pullRequests.length} pull requests with the sha ${currentSha}.`)
 
     if (!pullRequests.length) {
-      core.setFailed(
+      throw new Error(
         `No pull requests found for ${github.context.repo.owner}/${github.context.repo.repo}@${currentSha}, Github Action failed.`,
       )
     }
 
     let pullRequest = pullRequests[0]
     setOutput('number', pullRequest.number)
-    setOutput('title', title || '')
-    setOutput('body', body || '')
+    // setOutput('title', title || '')
+    // setOutput('body', body || '')
     setOutput('url', pullRequest.html_url || '')
     setOutput('base-ref', pullRequest.base?.ref || '')
     setOutput('base-sha', pullRequest.base?.sha || '')
